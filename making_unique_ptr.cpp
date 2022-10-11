22b68d3a6761413d55b3a85a114e58c2e9325f1a
chorockuin <chorockuin@gmail.com>
2022-10-11 03:04:24 +0000

    Update making_unique_ptr
    add lambda freer

--- making_unique_ptr.cpp
+++ making_unique_ptr.cpp
@@ -35,8 +35,15 @@ struct default_delete {
 template<typename T, typename D=default_delete<T>>
 class unique_ptr {
     T* p;
+    // D del;
 public:
     explicit unique_ptr(T* p) : p(p) {}
+    // c++20 이전 버전에서 소멸자를 람다로 구현하려면, 클래스 멤버에 D 타입의 del을 추가한다
+    // 그리고 아래처럼 생성자를 만들면 d가 복사 생성되는데, 람다가 복사 생성자는 지원하므로 사용 가능하다
+    // 그런데 쓸 데 없이 D 타입의 del을 추가해서 공간을 차지하기 때문에, 이전에 구현했던 compressed_pair를 활용하면 좋다
+    // unique_ptr(T* p, const D& d): p(p), del(d) {}
+    // move도 지원하려면 아래와 같이
+    // unique_ptr(T* p, D& d): p(p), del(std::move(d)) {} 
     ~unique_ptr() {
         if (p) {
             D del;
@@ -71,7 +78,31 @@ void making_unique_ptr1() {
     unique_ptr<int, Freer> p4(static_cast<int*>(malloc(sizeof(int))));
 }
 
+void making_unique_ptr2() {
+    // 삭제자에 람다를 넘기고 싶은데, 람다는 타입이 아닌 함수 객체이므로 아래 코드는 에러난다
+    // unique_ptr<int, [](int* p) {free(p);}> p1(static_cast<int*>(malloc(sizeof(int))));
+
+    // 아래처럼 decltype()으로 타입을 넘기면 될까?
+    // c++20 이전에는 아래처럼 하면 될 것 같은데,
+    // 그래도 안되는 이유는 람다는 디폴트 생성자가 없기 때문에 
+    // ~unique_ptr() {
+    //     if (p) {
+    //         D del; // 이것이 실패하고, 그래서 안된다
+    //         del(p);
+    //     }
+    // }
+    // 그래서 결국 삭제자를 인자로 받는 unique_ptr의 생성자를 따로 하나 더 만들 수 밖엔 없다
+    // auto del = [](int* p) {free(p);};
+    // unique_ptr<int, decltype(del)> p1(static_cast<int*>(malloc(sizeof(int))), del);
+    // move를 지원하려면 동일하게 unique_ptr의 생성자를 따로 하나 더 만든다
+    // unique_ptr<int, decltype(del)> p2(static_cast<int*>(malloc(sizeof(int))), std::move(del));
+
+    // c++20 이전까지 람다는 평가되지 않는 표현식에 넣을 수 없었기 때문에 안되었었다. c++20 부터 된다
+    unique_ptr<int, decltype([](int* p) {free(p);})> p1(static_cast<int*>(malloc(sizeof(int))));
+}
+
 void making_unique_ptr() {
     basic();
     making_unique_ptr1();
+    making_unique_ptr2();
 }
